fcdcbdbfb5713d12feb5b3dd214f7feb52cd7d52
Aryan Tiwari <91017115+MrAryan8728@users.noreply.github.com>
2024-02-16 09:35:10 +0530

    Updated UI

--- cryptoraise/Components/Cards.jsx
+++ cryptoraise/Components/Cards.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 
 const Cards = ({name}) => {
   return (
-    <div className=' w-[15rem] rounded-xl shadow-lg flex flex-col m-3 bg-white h-fit'>
+    <div className=' w-[15rem] rounded-xl shadow-black shadow-2xl flex flex-col m-3 bg-white h-fit '>
         <img 
-        className=' w-[100%] rounded-sm'
+        className=' w-[100%] rounded-lg p-2'
         alt='Campaign Image' 
         src='https://picsum.photos/200/150'></img>
-        <h1 className=' m-1 font-bold text-lg font-sans'>{name}</h1>
-        <p className=' m-1 font-serif'>This is the description of the card to be given</p>
+        <h1 className=' m-1 font-bold text-lg font-sans p-2'>{name}</h1>
+        <p className=' m-1 font-serif p-2'>This is the description of the card to be given</p>
         {/* To-do: Progress Bar */}
         <a href='#' className=' m-2 text-center w-[90%] bg-blue-500 rounded-md font-bold p-2 hover:bg-blue-600 text-white'>Contribute</a>
     </div>
