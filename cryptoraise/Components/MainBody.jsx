6fd40626db5512ade115d30b1940d4f259bd4222
Aryan Tiwari <91017115+MrAryan8728@users.noreply.github.com>
2024-02-20 18:49:08 +0530

    Added Init form and Bug fixed

--- cryptoraise/Components/MainBody.jsx
+++ cryptoraise/Components/MainBody.jsx
@@ -6,7 +6,7 @@ const MainBody = () => {
     <div>
       <div className="project-image w-[100%] h-[25rem]" style={{ backgroundImage: "url(https://picsum.photos/900/300)", backgroundRepeat: "no-repeat", backgroundSize: "100% 100%", objectFit: "cover" }}>
         {/* To-do : Description  */}
-        <div className="font-bold text-white relative top-[35%] left-[45%] text-3xl" style={{ filter: "brightness(100%)" }}>Check</div>
+        <div className="font-bold text-white text-3xl" style={{ filter: "brightness(100%)" }}>Check</div>
         <div className="btn-container flex gap-6 justify-center relative top-[45%]" style={{ filter: "brightness(100%)" }}>
           <div>
             <Link href = "/InitCamp"><button className="rounded p-3 font-bold bg-gradient-to-r from-cyan-500 to-blue-800  text-white hover:bg-blue-800" style={{ filter: "brightness(100%)" }}>Start a Project</button></Link>
--- cryptoraise/app/InitCamp/page.jsx
+++ cryptoraise/app/InitCamp/page.jsx
@@ -2,110 +2,106 @@ import React from "react";
 
 const InitCamp = () => {
   return (
-    <div className="container m-auto h-[80vh] border-blue-400 border-8 ">
-      <h1 className="font-bold text-xl text-center my-4 ">Campaign Details</h1>
-      <div className="mx-auto">
-        <div className="px-2 w-1/2">
-          <div className="mb-4">
-            <label htmlFor="name" className="leading-7 text-sm text-gray-600">
-              Campaign Name
-            </label>
-            <input
-              // onChange={handleChange}
-              // value={name}
-              type="text"
-              id="name"
-              name="name"
-              className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-            />
-          </div>
-        </div>
-        <div className="px-2 w-1/2">
-          <div className="mb-4">
-            <label htmlFor="text" className="leading-7 text-sm text-gray-600">
-              Description
-            </label>
-            <input
-              // onChange={handleChange}
-              // value={name}
-              type="text"
-              id="name"
-              name="name"
-              className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-            />
-          </div>
+    <div className="container h-[full] w-[60%] m-auto">
+      <h1 className="font-bold text-xl my-4 text-center">Campaign Details</h1>
+      {/* <div className=""> */}
+      <div className="px-2">
+        <div className="mb-4">
+          <label htmlFor="name" className="leading-7 text-sm text-gray-600">
+            Campaign Name
+          </label>
+          <input
+            // onChange={handleChange}
+            // value={name}
+            type="text"
+            id="name"
+            name="name"
+            className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+          />
         </div>
       </div>
-      <div className="mx-auto ">
-        <div className="px-2 w-1/2">
-          <div className="mb-4">
-            <label htmlFor="amount" className="leading-7 text-sm text-gray-600">
-              Amount to be raised
-            </label>
-            <input
-              type="number"
-              min={1}
-              // onChange={handleChange}
-              // value={phone}
-              id="amount"
-              name="number"
-              className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-            />
-          </div>
-        </div>
-        <div className="px-2 w-1/2">
-          <div className="mb-4">
-            <label
-              htmlFor="image"
-              className="leading-7 text-sm text-gray-600"
-            >
-              Select project's image
-            </label>
-            <input
-              type="file"
-              id="prj-img"
-              name="prj-img"
-              // onChange={handleChange}
-              // value={pincode}
-              className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-            />
-          </div>
+      <div className="px-2">
+        <div className="mb-4">
+          <label htmlFor="text" className="leading-7 text-sm text-gray-600">
+            Description
+          </label>
+          <input
+            // onChange={handleChange}
+            // value={name}
+            type="text"
+            id="name"
+            name="name"
+            className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+          />
         </div>
       </div>
-      <div className="mx-auto ">
-        <div className="px-2 w-1/2">
-          <div className="mb-4">
-            <label htmlFor="category" className="leading-7 text-sm text-gray-600">
-              Category
-            </label>
-            <input
-              type="text"
-              // value={state}
-              // onChange={handleChange}
-              id="category"
-              name="category"
-              className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-            />
-          </div>
-        </div>
-        <div className="px-2 w-1/2">
-          <div className="mb-4">
-            <label htmlFor="country" className="leading-7 text-sm text-gray-600">
-              Country of Origin
-            </label>
-            <input
-              type="text"
-              // onChange={handleChange}
-              // value={city}
-              id="country"
-              name="country"
-              className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-            />
-          </div>
+      <div className="px-2">
+        <div className="mb-4">
+          <label htmlFor="amount" className="leading-7 text-sm text-gray-600">
+            Amount to be raised
+          </label>
+          <input
+            type="number"
+            min={1}
+            // onChange={handleChange}
+            // value={phone}
+            id="amount"
+            name="number"
+            className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+          />
         </div>
       </div>
-      <div className="btn cursor-pointer bg-blue-500">Submit</div>
+      <div className="px-2">
+        <div className="mb-4">
+          <label
+            htmlFor="image"
+            className="leading-7 text-sm text-gray-600"
+          >
+            Select project's image
+          </label>
+          <input
+            type="file"
+            id="prj-img"
+            name="prj-img"
+            // onChange={handleChange}
+            // value={pincode}
+            className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+          />
+        </div>
+      </div>
+      <div className="px-2">
+        <div className="mb-4">
+          <label htmlFor="category" className="leading-7 text-sm text-gray-600">
+            Category
+          </label>
+          <input
+            type="text"
+            // value={state}
+            // onChange={handleChange}
+            id="category"
+            name="category"
+            className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+          />
+        </div>
+      </div>
+      <div className="px-2">
+        <div className="mb-4">
+          <label htmlFor="country" className="leading-7 text-sm text-gray-600">
+            Country of Origin
+          </label>
+          <input
+            type="text"
+            // onChange={handleChange}
+            // value={city}
+            id="country"
+            name="country"
+            className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+          />
+        </div>
+        <div className="button text-center p-3 rounded w-[30%] translate-x-[120%] font-bold">Submit</div>
+      </div>
     </div>
+    // </div>
   );
 };
 
