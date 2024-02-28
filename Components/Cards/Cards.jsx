fa8f02acb2cc0956d9680d834f5c04b00d14dd32
Aryan Tiwari <91017115+MrAryan8728@users.noreply.github.com>
2024-02-28 12:52:14 +0530

    changes

--- Components/Cards/Cards.jsx
+++ Components/Cards/Cards.jsx
@@ -3,7 +3,8 @@ import Card from "../Card";
 const page = () => {
   return (
     <div className="cards">
-      <div className=" grid grid-cols-3 gap-6 ml-[12%] py-[12%]">
+    <div className=" flex items-center justify-center">
+      <div className=" grid grid-cols-3 gap-6">
         <Card name={"Treatment of cancer"} />
         <Card name={"Life-style Fit Club"} />
         <Card name={"Renewable Energy"} />
@@ -11,6 +12,7 @@ const page = () => {
         <Card name={"Sustainable Development"} />
         <Card name={"Earthquake Relief"} />
       </div>
+    </div>
     </div>
   );
 };
