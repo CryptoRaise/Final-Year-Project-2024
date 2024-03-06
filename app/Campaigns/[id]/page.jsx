4c84356bb31fc63c4236b5eb9a6904431e976ec0
Rishabh-Kumar-Sharma <rishabhkumarsharma2002@gmail.com>
2024-03-06 13:34:26 +0530

    Added some back-end components

--- app/Campaigns/[id]/page.jsx
+++ app/Campaigns/[id]/page.jsx
@@ -1,22 +1,22 @@
-import React from 'react'
-import data from '@/data.js'
+import React from "react";
+import data from "@/data.js";
 
 const page = ({ params }) => {
-  const filterData = data.filter((item) => item.id == params.id)
+  const filterData = data.filter((item) => item.id == params.id);
   return (
     <div>
-      {
-        filterData.map((value) => {
-          return <div className=' grid grid-cols-3'>
-          <div>
-            <p>{value.title}</p>
-            <p>{value.text}</p>
+      {filterData.map((value) => {
+        return (
+          <div className=" grid grid-cols-3">
+            <div>
+              <p>{value.title}</p>
+              <p>{value.text}</p>
+            </div>
           </div>
-          </div>
-        })
-      }
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
