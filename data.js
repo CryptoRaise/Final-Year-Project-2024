0f942a9b8d8ae8888d9d711637ca2267fa5abf60
Aryan Tiwari <91017115+MrAryan8728@users.noreply.github.com>
2024-03-06 13:19:08 +0530

    Dynamic routing completed

--- data.js
+++ data.js
@@ -1,47 +1,56 @@
 export default [
     {
-        img: "https://picsum.photos/200/150",
-        title: "Create a website using Html CSS and JavaScript",
-        text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
-    },
-    {
-        img: "https://picsum.photos/200/150",
-        title: "Complete portfolio website tutorial",
-        text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
-    },
-    {
-        img: "https://picsum.photos/200/150",
-        title: "Bootstrap 5 Complete tutorial",
-        text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
-    },
-    {
-        img: "https://picsum.photos/200/150",
-        title: "UIkit Complete tutorial",
-        text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
-    },
-    {
+        id:1,
         img: "https://picsum.photos/200/150",
         title: "Tailwind CSS card design",
         text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
     },
     {
+        id:2,
         img: "https://picsum.photos/200/150",
         title: "Reactjs tutorial for beginner",
         text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
     },
     {
+        id:3,
         img: "https://picsum.photos/200/150",
         title: "Nextjs crash course",
         text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
     },
     {
+        id:4,
         img: "https://picsum.photos/200/150",
         title: "Create a website using Tailwind CSS",
         text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
     },
     {
+        id:5,
         img: "https://picsum.photos/200/150",
         title: "Create an one page website using ReactJS",
+        text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
+    },
+    {
+        id:9,
+        img: "https://picsum.photos/200/150",
+        title: "Create a website using Html CSS and JavaScript",
+        text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
+    },
+    {
+        id:8,
+        img: "https://picsum.photos/200/150",
+        title: "Complete portfolio website tutorial",
+        text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
+    },
+    {
+        id:7,
+        img: "https://picsum.photos/200/150",
+        title: "Bootstrap 5 Complete tutorial",
+        text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
+    },
+    {
+        id:6,
+        img: "https://picsum.photos/200/150",
+        title: "UIkit Complete tutorial",
         text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
     },
 ]
