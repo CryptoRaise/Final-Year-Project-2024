a5b24879da290d44d6b0107b9fb14d7beb561da7
Rishabh-Kumar-Sharma <rishabhkumarsharma2002@gmail.com>
2024-03-07 00:54:26 +0530

    Hardhat Configuration completed

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,8 +3,12 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
     solidity: "0.8.24",
-    defaultNetwork: "hardhat",
+    defaultNetwork: "sepolia",
     networks: {
         hardhat: {},
+        sepolia: {
+            url: "https://eth-sepolia.g.alchemy.com/v2/ovCG6hx9y_hmWXXcbb_9OwrnMIVPKbXJ",
+            accounts: ["d834a4eb29486d9d97cb0595ca34e9daaea7307e4031a4aaaa3e8f6efd4e86fe"]
+        }
     },
 };
