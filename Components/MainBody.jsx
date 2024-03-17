7c970a89ffc72b9912b0153653fbd498a8bb12e4
Rishabh-Kumar-Sharma <rishabhkumarsharma2002@gmail.com>
2024-03-17 16:15:03 +0530

    Minor UI Improvements

--- Components/MainBody.jsx
+++ Components/MainBody.jsx
@@ -3,14 +3,13 @@ import React from "react";
 import Cards from "./Cards/Cards";
 import ImageBar from "./ImageBar/ImageBar";
 import Faq from "./FAQ";
-import Image from "next/image";
 
 const MainBody = () => {
   return (
     <div className="MainBody">
       <ImageBar />
       <Cards name="Recommended Projects" />
-      <div className=" w-full h-96 my-10">
+      <div className=" w-full h-96 mt-16 mb-9">
         <img src="./banner.jpg" className="w-full h-full object-c"/>
       </div>
       <Cards name="Featured Projects" />
